177472f6008bcbe6c32131498cc62ea8c43c72bd
tetakashi <83709530+tetakashi@users.noreply.github.com>
2021-06-15 11:02:51 +0800

    Add files via upload

---
+++ turtlecoin.herominers.bat.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o turtlecoin.herominers.com:10380 -u TRTLv2xmujTURFmSh2zMzKAaTPE3LYudH2rvY7EnLEU6JEpvqbGBq1c6oUcQV7c8DbVBzE3wCNs6TcpFiKpQiZyd1W7n7yeEdfr -p Tetakashi -a argon2/chukwav2 -k 
+pause
